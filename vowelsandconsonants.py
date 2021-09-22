68ed15fb6c391551ec64bf5bf6416be097fcb3d9
chandralekhasingasani <chandralekha.s@nutanix.com>
2021-09-22 23:02:40 +0530

    coding

---
+++ vowelsandconsonants.py
@@ -0,0 +1,11 @@
+#How to calculate the number of vowels and consonants in a string?
+
+def calculate(str):
+    vowels=0
+    consonants=0
+    for i in str:
+        if i in ['a','e','i','o','u']:
+            vowels+=1
+        else:
+            consonants+=1
+    return (vowels,consonants)
