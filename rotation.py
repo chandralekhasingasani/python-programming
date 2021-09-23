e804e64789981449c1f87189435fc2948cc1086d
chandralekhasingasani <chandralekha.s@nutanix.com>
2021-09-23 06:30:21 +0530

    coding

--- rotation.py
+++ rotation.py
@@ -1,7 +1,7 @@
 #How to verify if two strings are a rotation mutually
 
 def isrotated(str1,str2):
-    if len(str1) == len(str2):
+    if len(str1) != len(str2):
         return False
     else:
         org=str1+str1
