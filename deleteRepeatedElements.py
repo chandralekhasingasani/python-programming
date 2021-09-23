d821238bd5447161b460c297b1d31754b19b1b2a
chandralekhasingasani <chandralekha.s@nutanix.com>
2021-09-23 11:51:40 +0530

    coding

---
+++ deleteRepeatedElements.py
@@ -0,0 +1,14 @@
+#How to delete the repeated elements in an integer array?
+
+def deleteRepeatedElements(list):
+    dict={}
+    for i in range(0,len(list)):
+        if list[i] in dict:
+            list.remove(i)
+        else:
+            dict[i]=1
+    return list
+
+print(deleteRepeatedElements[2,3,4,2])
+
+
--- repeatingElementsInlist.py
+++ repeatingElementsInlist.py
@@ -3,10 +3,11 @@
 def matchingElements(list):
     a=set()
     for i in range(0,len(list)-1):
-        if list[i] == list[i+1]:
-            a.add(i)
+        for j in range(i+1,len(list)):
+            if list[i] == list[j]:
+                a.add(list[i])
     return a
 
-print(matchingElements([20,3,4,5,6,20]))
+print(matchingElements([20,3,4,5,6,6,20]))
 
 
