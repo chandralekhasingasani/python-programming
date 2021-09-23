b00fae2b0a48de92ce2d112266d0ab868ce4ca06
chandralekhasingasani <chandralekha.s@nutanix.com>
2021-09-23 18:33:48 +0530

    coding

--- reverseLinkedlist.py
+++ reverseLinkedlist.py
@@ -1,26 +1,45 @@
 class List:
-    first=last=None
+    first=None
+    last=None
     def __init__(self):
         self.next=None
 
     def addElementInListAtEnd(self,item):
         temp=List()
         temp.item=item
-        if first == None:
-            first=last=temp
+        if List.first is None:
+            List.first=temp
+            List.last=temp
         else:
-            temp.next=last
-            last.next=temp
-            last=temp
+            List.last.next=temp
+            List.last=temp
 
     def printElements(self):
-        temp=first
-        while temp != None:
+        temp=List.first
+        while temp is not None:
             print(temp.item)
             temp=temp.next
 
+    def reverseLinkedList(self):
+        temp=None
+        curl=None
+
+        while List.first is not None:
+            temp=List.first
+            List.first=List.first.next
+            temp.next=curl
+            curl=temp
+
+        List.first=curl
+        return List.first
+
 l=List()
 l.addElementInListAtEnd(30)
+l.addElementInListAtEnd(20)
+l.addElementInListAtEnd(10)
+l.printElements()
+l.reverseLinkedList()
+l.printElements()
 
 
 
