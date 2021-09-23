96c7d1e0ec7a1036d6d37658c0d599c8042f86e4
chandralekhasingasani <chandralekha.s@nutanix.com>
2021-09-24 00:27:17 +0530

    coding

--- problem-solving/reverseNumber.py
+++ problem-solving/reverseNumber.py
@@ -1,9 +1,8 @@
 def reverseNum(num):
     rev=0
     while num>0:
-        temp=num%10
         rev=(rev*10)+(num%10)
-        num=num/10
+        num=int(num/10)
     return rev
 
 print(reverseNum(300))
