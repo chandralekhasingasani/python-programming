f7008e0b414f33869e5b21197aae960643502bae
chandralekhasingasani <chandralekha.s@nutanix.com>
2021-09-23 19:32:59 +0530

    coding

---
+++ problem-solving/fibonnaci.py
@@ -0,0 +1,14 @@
+#Compute the first five Fibonacci numbers.
+
+def fibbo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    fib=1
+    list=[0,1]
+    for i in range(1,n-2+1):
+        list.append(list[i]+list[i-1])
+    return list
+
+print(fibbo(5))
--- problem-solving/primenumbers.py
+++ problem-solving/primenumbers.py
@@ -1,9 +1,9 @@
 def primenumber(num):
     if num == 1 or num == 2:
         return True
-    for i in range(2,num/2+1):
+    for i in range(2,int(num/2+1)):
         if num % i == 0:
             return False
     return True
 
-print(primenumber(100))
+print(primenumber(7))
